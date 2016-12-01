814d1a6f9ed2a360374d46dcfda5d460f1fb4856
Alex <zimmalex6@gmail.com>
2016-12-02 00:53:09 +0200

    liftin step up lessons

--- js/app.js
+++ js/app.js
@@ -1,178 +1,303 @@
 //utils
 function formatDate(date) {
-    return date.toLocaleDateString();
+  return date.toLocaleDateString();
 }
 
 
+function Components(props) {
 
-function Components(props){
+  const {author, text, date} = {...props}
 
-    const {author, text, date} = {...props}
-
-    return (
-        <div className="comment">
-            <div className="comment__user">
-                <div className="user__name">{author.name}</div>
-                <div className="user__img"><img src={author.avatarUrl} alt=""/></div>
-            </div>
-            <div className="comment__text">{text}</div>
-            <div className="comment__date">{formatDate(date)}</div>
-        </div>
-    )
+  return (
+    <div className="comment">
+      <div className="comment__user">
+        <div className="user__name">{author.name}</div>
+        <div className="user__img"><img src={author.avatarUrl} alt=""/></div>
+      </div>
+      <div className="comment__text">{text}</div>
+      <div className="comment__date">{formatDate(date)}</div>
+    </div>
+  )
 }
 
-function App (){
-    return (
-            <div>
-                <Clock/>
-                <Clock/>
-                <Clock/>
-                <Clock/>
-            </div>
-        )
+function App() {
+  return (
+    <div>
+      <Clock/>
+      <Clock/>
+      <Clock/>
+      <Clock/>
+    </div>
+  )
 }
 
 const comment = {
-    date: new Date(),
-    text: 'I hope you enjoy learning React!',
-    author: {
-        name: 'Hello Kitty',
-        avatarUrl: 'http://placekitten.com/g/200/300'
-    }
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/200/300'
+  }
 };
 
 class Clock extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            date: new Date()
-        };
-    }
+    this.state = {
+      date: new Date()
+    };
+  }
 
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
 
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
-    tick(){
-        this.setState({date: new Date()})
-    }
+  tick() {
+    this.setState({date: new Date()})
+  }
 
-    render (){
-        return (
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    )
+  }
 }
 
-class ButtonClick extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            isToggleOn : true
-        };
+class ButtonClick extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true
+    };
 
-    }
+  }
 
-    handleClick(){
+  handleClick() {
 
-        this.setState(prevState =>({
-            isToggleOn : !prevState.isToggleOn
-        }))
-    }
+    this.setState(prevState =>({
+      isToggleOn: !prevState.isToggleOn
+    }))
+  }
 
-    render(){
-        return (
-            <button onClick={this.handleClick.bind(this)}>
-                {!this.state.isToggleOn ? 'No' : 'Yes'}
-            </button>
-        )
-    }
+  render() {
+    return (
+      <button onClick={this.handleClick.bind(this)}>
+        {!this.state.isToggleOn ? 'No' : 'Yes'}
+      </button>
+    )
+  }
 }
 
 class LoginControl extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {isLoggendIn : false};
-        this.handleLogoutClick = this.handleLogoutClick.bind(this);
-        this.handleLoginClick = this.handleLoginClick.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {isLoggendIn: false};
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggendIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggendIn: false});
+  }
+
+  render() {
+    const isLoggendIn = this.state.isLoggendIn;
+
+    let button;
+
+    if (isLoggendIn) {
+      button = <button onClick={this.handleLogoutClick}>Logout</button>
+
+    } else {
+
+      button = <button onClick={this.handleLoginClick}>Login</button>
     }
 
-    handleLoginClick(){
-        this.setState({isLoggendIn : true});
-    }
-
-    handleLogoutClick(){
-        this.setState({isLoggendIn : false});
-    }
-
-    render(){
-        const isLoggendIn = this.state.isLoggendIn;
-
-        let button;
-
-        if(isLoggendIn){
-            button = <button onClick={this.handleLogoutClick} >Logout</button>
-
-        }else{
-
-            button = <button onClick={this.handleLoginClick}>Login</button>
-        }
-
-        return (button)
-    }
+    return (button)
+  }
 }
 
-function WarningBanner(props){
-    if(!props.warn){
-        return null
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null
+  }
+
+  return (
+    <div className="warning">
+      Warning!
+    </div>
+  )
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: true};
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(prevState => ({
+      showWarning: !prevState.showWarning
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning}/>
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Show' : 'Hide'}
+        </button>
+      </div>
+    )
+  }
+}
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: 'some value for input'
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChange(e) {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className="class" onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        </label>
+      </form>
+    )
+  }
+}
+
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
+
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(value, convert) {
+  const input = parseFloat(value);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onChange(e.target.value);
+  }
+
+  render() {
+    const value = this.props.value;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={value}
+               onChange={this.handleChange}/>
+      </fieldset>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = {value: '', scale: 'c'};
+  }
+
+  handleCelsiusChange(value) {
+    this.setState({scale: 'c', value});
+  }
+
+  handleFahrenheitChange(value) {
+    this.setState({scale: 'f', value});
+  }
+
+  render() {
+    const scale = this.state.scale;
+    const value = this.state.value;
+
+    const celsius = scale === 'f' ? tryConvert(value, toCelsius) : value;
+    const fahrenheit = scale === 'c' ? tryConvert(value, toFahrenheit) : value;
 
     return (
-        <div className="warning">
-            Warning!
-        </div>
-    )
+      <div>
+        <TemperatureInput
+          scale="c"
+          value={celsius}
+          onChange={this.handleCelsiusChange}/>
+        <TemperatureInput
+          scale="f"
+          value={fahrenheit}
+          onChange={this.handleFahrenheitChange}/>
+        <BoilingVerdict
+          celsius={parseFloat(celsius)}/>
+      </div>
+    );
+  }
 }
-
-class Page extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {showWarning : true};
-        this.handleToggleClick = this.handleToggleClick.bind(this);
-    }
-
-    handleToggleClick(){
-        this.setState( prevState => ({
-            showWarning : !prevState.showWarning
-        }))
-    }
-
-    render (){
-        return (
-            <div>
-                <WarningBanner warn={this.state.showWarning} />
-                <button onClick={this.handleToggleClick}>
-                    {this.state.showWarning ? 'Show' : 'Hide'}
-                </button>
-            </div>
-            )
-    }
-
-}
-
 
 ReactDOM.render(
-    <Page />,
-    document.getElementById('root')
+  <Calculator />,
+  document.getElementById('root')
 );
